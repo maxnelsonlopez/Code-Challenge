c390d42c1afaa91a869ab60fe03af01752489b6a
maxnelsonlopez <manlopezre@unal.edu.co>
2022-06-21 23:37:25 -0500

    descargar_datos carga el csv
    Ahora ya funciona descargar_datos. Utiliza las url de los csv y crea los archivos en sus respectivas rutas.

--- src/main.py
+++ src/main.py
@@ -2,26 +2,72 @@
 
 # Press ⌃R to execute it or replace it with your code.
 # Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+import csv
+import os
+from os.path import exists
+from time import strftime, gmtime
+
 import requests
 
 
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+
+
 def descargar_datos():
-    #Obtener los 3 archivos de fuente utilizando la libreria
-    #requests y almacenarse de forma local
-    #Las Urls pueden cambiar en un futuro
+    # Obtener los 3 archivos de fuente utilizando la libreria
+    # requests y almacenarse de forma local
+    # Las Urls pueden cambiar en un futuro
 
-    #Necesito las Urls,hacer un request para cada una y almacenarla
-    #En objetos request, luego llamo json() y de json() lo pasaré a csv
+    # Necesito las Urls,hacer un request para cada una y almacenarla
+    # En objetos request, luego llamo json() y de json() lo pasaré a csv
 
-    #TODO Datos Argentina Museos
-    datos_argentina_museos = requests.get('https://datos.cultura.gob.ar/dataset/37305de4-3cce-4d4b-9d9a-fec3ca61d09f/archivo/4207def0-2ff7-41d5-9095-d42ae8207a5d')
-    print(datos_argentina_museos.headers)
+    # Descargar Datos Argentina Museos
+    url_museos = 'https://datos.cultura.gob.ar/dataset/37305de4-3cce-4d4b-9d9a-fec3ca61d09f/resource/4207def0-2ff7' \
+                 '-41d5-9095-d42ae8207a5d/download/museos_datosabiertos.csv '
 
-    #TODO Datos Argentina Salas de Cine
-    #TODO Datos Argentina Bibliotecas Populares
+    # Creando la ruta y el nombre de archivo del csv
+    categoria = "museos"
+    archivo_museos = crear_archivo(categoria)
+    # Descargando los datos en el archivo
+    cargar_a_archivo(archivo_museos, url_museos)
+    # Descargar Datos Argentina Salas de Cine
+    url_cines = 'https://datos.cultura.gob.ar/dataset/37305de4-3cce-4d4b-9d9a-fec3ca61d09f/resource/392ce1a8-ef11' \
+                '-4776-b280-6f1c7fae16ae/download/cine.csv '
+    categoria = "cines"
+    archivo_cines = crear_archivo(categoria)
+    cargar_a_archivo(archivo_cines, url_cines)
+    # Descargar Datos Argentina Bibliotecas Populares
+    url_bibliotecas = 'https://datos.cultura.gob.ar/dataset/37305de4-3cce-4d4b-9d9a-fec3ca61d09f/resource/01c6c048' \
+                      '-dbeb-44e0-8efa-6944f73715d7/download/biblioteca_popular.csv '
+    categoria = "bibliotecas"
+    archivo_bibliotecas = crear_archivo(categoria)
+    cargar_a_archivo(archivo_bibliotecas, url_bibliotecas)
+
+
+def cargar_a_archivo(archivo, url):
+    with requests.get(url, stream=True) as datos_argentina_categoria:
+        lines = (line.decode('utf-8') for line in datos_argentina_categoria.iter_lines())
+        for row in csv.reader(lines):
+            spamwriter = csv.writer(archivo, delimiter=';',
+                                    quotechar='|', quoting=csv.QUOTE_MINIMAL)
+            spamwriter.writerow(row)
+
+
+def crear_archivo(categoria):
+    #Crea la ruta con el formato especificado en el pdf y luego crea el archivo
+    ruta_archivo = "{}/".format(categoria) + strftime("%Y-%B", gmtime()) + "/"
+    nombre_archivo = ruta_archivo + "{categoria}-{date}.csv".format(categoria=categoria, date=strftime("%d-%m-%Y"))
+    print(nombre_archivo)
+    os.makedirs(ruta_archivo, exist_ok=True)
+    print(os.getcwd())
+    existe = exists(nombre_archivo)
+    if existe:
+        os.remove(nombre_archivo)
+    archivo = open(nombre_archivo, "w")
+    return archivo
+
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
